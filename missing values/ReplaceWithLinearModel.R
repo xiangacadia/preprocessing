2305df0b4f2f48117868be7b435d7cc271f4f43e
xiangacadia <xiangjiang.acadia@gmail.com>
2014-05-05 15:44:32 -0300

    basic function is done

--- missing values/ReplaceWithLinearModel.R
+++ missing values/ReplaceWithLinearModel.R
@@ -17,24 +17,40 @@ endRow <- nrow(data)
 result <- data.frame()
 
 for(rowNum in c(startRow : endRow)){
-
+    
+    #rowNum <- 4
+    
     ## get data for each series
     series <- as.integer(data[rowNum,])
     
-    ## replace un-numeric values with NA
-    # series <- replace(series, c(1:endColumn)[series==NA_str], NA)
-    
-    ## to-do: there should be more check for numeric values in case there is other NA strings
-    
-    ## build linear model and get prediction
-    prediction <- predict(lm(series[startColumn:endColumn] ~ c(startColumn:endColumn)), data.frame(c(startColumn:endColumn)), se.fit = TRUE)
-    
-      
-    ## put data together
-    complete_series <- series
-    #names(complete_series) <- names(series)
-    complete_series[is.na(series)[startColumn:endColumn]] <- prediction$fit[is.na(series)[startColumn:endColumn]]
+    ## test if all values in series are NA
+    if(length(series[!is.na(series)[startColumn:endColumn]]) == 0){
+        complete_series <- rep(0, endRow - startRow)
+    }else{
+        ## test if all values in the series is not NA
+        if(length(series[is.na(series)[startColumn:endColumn]]) == 0){
+            complete_series <- series
+        }else{
+            
+            ## replace un-numeric values with NA
+            # series <- replace(series, c(1:endColumn)[series==NA_str], NA)
+            
+            ## to-do: there should be more check for numeric values in case there is other NA strings
+            
+            ## build linear model and get prediction
+            prediction <- predict(lm(series[startColumn:endColumn] ~ c(startColumn:endColumn)), data.frame(c(startColumn:endColumn)), se.fit = TRUE)
+            
+            ## put data together
+            complete_series <- series
+            #names(complete_series) <- names(series)
+            complete_series[is.na(series)][startColumn:endColumn] <- prediction$fit[is.na(series)[startColumn:endColumn]]
+        }
+    }
     
     ## rewrite the original dataset
     result <- rbind(result, complete_series)
+    
 }
+
+## rewrite the names of the columns
+names(result) <- names(data)
